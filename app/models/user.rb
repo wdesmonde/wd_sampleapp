e59922c0b3df740282e727439cd3206efed1b303
Wen Zilla <willazen@gmail.com>
2011-08-16 14:52:38 -0700

    added user model, made changes through section 6-2-3 of hartl

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  # from Rubular
+  email_regex = /\A([^@\s]+)@((?:[a-z0-9]+\.)+[a-z]{2,})\Z/i
+  # from Hartl.  this one doesn't work in Rubular.
+  # email_regex = /\A[\w+\-.]+@[a-z\d\-.]\.[a-z]+\Z/i
+
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex }
+  
+end
