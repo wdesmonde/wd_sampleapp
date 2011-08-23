dfc932e38ed3226374bad54a614e964ec06a7296
Wen Zilla <willazen@gmail.com>
2011-08-23 12:28:24 -0700

    added password encryption.  Up to Hartl 7-2-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 # == Schema Information
 #
 # Table name: users
@@ -31,5 +33,33 @@ class User < ActiveRecord::Base
     :presence => true,
     :confirmation => true,
     :length => { :within => 6..40 }  
+
+  before_save :encrypt_password
+
+  # return true if the user's password matches the submitted password
+  def has_password?(submitted_password)
+    # compare the encrypted password with the encrypted version of
+    #    submitted_password
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 
 end
