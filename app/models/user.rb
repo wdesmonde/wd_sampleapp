0a400300d5d99135f5aa418bdd69d1b207d64c85
Wen Zilla <willazen@gmail.com>
2011-08-23 09:30:49 -0700

    begin to implement user passwords through hartl 7-1-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,26 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   # from Rubular
   email_regex = /\A([^@\s]+)@((?:[a-z0-9]+\.)+[a-z]{2,})\Z/i
   # from Hartl.  this one doesn't work in Rubular.
   # email_regex = /\A[\w+\-.]+@[a-z\d\-.]\.[a-z]+\Z/i
 
-  validates :name, :presence => true,
-                   :length => { :maximum => 50 }
-  validates :email, :presence => true,
-                    :format   => { :with => email_regex },
-                    # Rails infers that :uniqueness should be true
-                    :uniqueness => { :case_sensitive => false }
-  
+  validates :name, 
+    :presence => true,
+    :length => { :maximum => 50 }
+  validates :email,
+    :presence => true,
+    :format   => { :with => email_regex },
+    # Rails infers that :uniqueness should be true
+    :uniqueness => { :case_sensitive => false }
+  # automatically create the virtual attribute "password confirmation"
+  validates :password, 
+    :presence => true,
+    :confirmation => true,
+    :length => { :within => 6..40 }  
+
 end
