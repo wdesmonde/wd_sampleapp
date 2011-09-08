6ba08f01a31a0e000546a8a6304f053339798c94
Wen Zilla <willazen@gmail.com>
2011-09-08 13:54:17 -0700

    started working on sessions and cookies for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+      params[:session][:password])
+      if user.nil?
+        # create an error message and re-render the sign-in form
+        flash.now[:error] = "Invalid email/password combination."
+        @title = "Sign in"
+        render 'new'
+      else
+        # sign-in the user and redirect to the user's show page
+        sign_in user
+        redirect_to user
+      end
   end
 
   def destroy
