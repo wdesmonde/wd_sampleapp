8653934be2b4b9e23a0173ff209a172e631a01af
Wen Zilla <willazen@gmail.com>
2011-09-09 11:54:22 -0700

    finished signing in using permanent sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,14 @@ module SessionsHelper
     @current_user = user
   end
 
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
   private
 
     def user_from_remember_token
