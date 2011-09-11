81ec05647e7843bfa2e72fb83ccccc092d642dae
Wen Zilla <willazen@gmail.com>
2011-09-11 13:45:29 -0700

    initial denial of access to edit if not signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -28,4 +29,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = "Edit User"
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
 end
